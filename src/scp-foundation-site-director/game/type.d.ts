d01a9eab8f1de94d358cb6f51e75face52a26154
lin onetwo <linonetwo012@gmail.com>
2024-01-27 22:07:34 +0800

    feat: load wasm game

---
+++ src/scp-foundation-site-director/game/type.d.ts
@@ -0,0 +1,3 @@
+declare module '$:/plugins/linonetwo/scp-foundation-site-director/game_bg.wasm' {
+  export * from './wasm/game_bg.wasm';
+}
--- src/scp-foundation-site-director/index.ts
+++ src/scp-foundation-site-director/index.ts
@@ -1,6 +1,7 @@
 import { widget as Widget } from '$:/core/modules/widgets/widget.js';
 import { IChangedTiddlers } from 'tiddlywiki';
-import init from './game/wasm/game';
+
+import { initSync } from './game/wasm/game';
 import './index.css';
 
 class ScpFoundationSiteDirectorGameWidget extends Widget {
@@ -16,9 +17,35 @@ class ScpFoundationSiteDirectorGameWidget extends Widget {
     });
     nextSibling === null ? parent.append(containerElement) : nextSibling.before(containerElement);
     this.domNodes.push(containerElement);
-    // wasm is bundled into tw using `game/tiddlywiki.files`
-    void init('$:/plugins/linonetwo/scp-foundation-site-director/game_bg.wasm');
+
+    const gameWasm = $tw.wiki.getTiddlerText('$:/plugins/linonetwo/scp-foundation-site-director/game_bg.wasm');
+    // wasm is bundled into tw using `game/tiddlywiki.files` as base64
+
+    if (gameWasm !== undefined) {
+      const wasmBuffer = loadWasmModuleFromBase64(gameWasm);
+      console.time('gameLoad'); // 384 ms
+      try {
+        initSync(wasmBuffer);
+      } catch (error) {
+        console.error('Game load with error', error);
+      }
+      console.timeEnd('gameLoad');
+    }
   }
+}
+
+function loadWasmModuleFromBase64(encodedWasm: string) {
+  // Decode the base64 string to binary data
+  console.time('wasmDecode'); // 157 ms
+  const binaryString = window.atob(encodedWasm);
+  const binaryLength = binaryString.length;
+  const bytes = new Uint8Array(binaryLength);
+  // Convert the binary string to a byte array
+  for (let index = 0; index < binaryLength; index++) {
+    bytes[index] = binaryString.codePointAt(index)!;
+  }
+  console.timeEnd('wasmDecode');
+  return bytes;
 }
 
 declare let exports: {

c2f749e2bd3e1221a670dc54a61e7e27bc5facc4
linonetwo <linonetwo012@gmail.com>
2024-02-01 00:38:36 +0800

    fix: pass buffer to initAsync instead of module to init in another thread

--- src/scp-foundation-site-director/index.ts
+++ src/scp-foundation-site-director/index.ts
@@ -32,11 +32,12 @@ class ScpFoundationSiteDirectorGameWidget extends GameWidget {
   }
 
   private startGame() {
+    this.setLoading(true);
     /**
      * Delay init next game to next tick (game loop frame), so last game (if any) can be destroyed first.
      * Without setTimeout, there will be `already borrowed: BorrowMutError` `RuntimeError: unreachable executed` error, when switch from one game to another.
      *
-     * // TODO: But sometimes there still be `Uncaught TypeError: wasm is undefined` error, when switch from one game to another, maybe switch is too fast? Change from `0` to `1000` fixes this, but it slowdown the game init. And this error throw on global can't be catch here.
+     * // TODO: But sometimes there still be `Uncaught TypeError: wasm is undefined` `Uncaught RuntimeError: unreachable` `Uncaught TypeError: Cannot read properties of undefined (reading 'wasm_bindgen__convert__closures__invoke0_mut__h9f5f8d8886b9ec6c')` `Uncaught TypeError: Cannot read properties of undefined (reading '__wbindgen_export_3')` error, when switch from one game to another, maybe switch is too fast? Change from `0` to `1000` some times still cannot fixes this, and it slowdown the game init. And this error throw on global can't be catch here.
      */
     setTimeout(async () => {
       await this.initializeGameCanvas();
@@ -59,14 +60,12 @@ class ScpFoundationSiteDirectorGameWidget extends GameWidget {
       const wasmBuffer = loadWasmModuleFromBase64(gameWasm);
       console.time('gameLoad'); // 384 ~ 1551 ms
       try {
-        this.setLoading(true);
-        const wasmModule = new WebAssembly.Module(wasmBuffer);
         destroy();
         /**
          * Can't use `initSync`, it cause error:
          * > RangeError: WebAssembly.Compile is disallowed on the main thread, if the buffer size is larger than 8MB. Use WebAssembly.compile, compile on a worker thread
          */
-        this.gameInstance = await initAsync(wasmModule);
+        this.gameInstance = await initAsync(wasmBuffer);
         startGame();
       } catch (error) {
         // https://users.rust-lang.org/t/getting-rid-of-this-error-error-using-exceptions-for-control-flow-dont-mind-me-this-isnt-actually-an-error/92209

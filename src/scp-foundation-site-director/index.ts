1e838e264b0b885b95970125df8408f0d2792adf
linonetwo <linonetwo012@gmail.com>
2024-02-03 00:42:44 +0800

    fix: manually create wasm context to prevent wasm reuse errors
    https://github.com/rustwasm/wasm-bindgen/issues/3818

--- src/scp-foundation-site-director/index.ts
+++ src/scp-foundation-site-director/index.ts
@@ -1,12 +1,12 @@
 import { IChangedTiddlers } from 'tiddlywiki';
 
-import initAsync, { destroy, InitOutput, setGamificationEvents, startGame, stopGame } from './game/wasm/game';
+import { getWasmContext, WasmContext } from './game/wasm/game';
 import './index.css';
 import { BasicGamificationEventTypes, IGamificationEvent } from 'src/tw-gamification/event-generator/GamificationEventTypes';
 import { GameWidget } from 'src/tw-gamification/game-wiki-adaptor/GameWidgetType';
 
 class ScpFoundationSiteDirectorGameWidget extends GameWidget {
-  gameInstance?: InitOutput;
+  wasmContext?: WasmContext;
   gameInitialized = false;
 
   refresh(_changedTiddlers: IChangedTiddlers) {
@@ -27,29 +27,23 @@ class ScpFoundationSiteDirectorGameWidget extends GameWidget {
     nextSibling === null ? parent.append(containerElement) : nextSibling.before(containerElement);
     this.domNodes.push(containerElement);
     // TODO: load assets from asset sub-plugin, and push list and item to game by call rust function
-    this.startGame();
+    void this.startGame();
     // TODO: handle destroy using https://github.com/Jermolene/TiddlyWiki5/discussions/5945#discussioncomment-8173023
   }
 
-  private startGame() {
+  private async startGame() {
     this.setLoading(true);
-    /**
-     * Delay init next game to next tick (game loop frame), so last game (if any) can be destroyed first.
-     * Without setTimeout, there will be `already borrowed: BorrowMutError` `RuntimeError: unreachable executed` error, when switch from one game to another.
-     *
-     * // TODO: But sometimes there still be `Uncaught TypeError: wasm is undefined` `Uncaught RuntimeError: unreachable` `Uncaught TypeError: Cannot read properties of undefined (reading 'wasm_bindgen__convert__closures__invoke0_mut__h9f5f8d8886b9ec6c')` `Uncaught TypeError: Cannot read properties of undefined (reading '__wbindgen_export_3')` error, when switch from one game to another, maybe switch is too fast? Change from `0` to `1000` some times still cannot fixes this, even the game should have been stopped. And it slowdown the game init. And this error throw on global can't be catch here.
-     */
-    setTimeout(async () => {
-      await this.initializeGameCanvas();
-      if (this.gameInitialized) {
-        this.popGamificationEvents();
-      }
-    }, 0);
+    await this.initializeGameCanvas();
+    if (this.gameInitialized) {
+      this.popGamificationEvents();
+    }
   }
 
   public destroy(): void {
     super.destroy();
-    stopGame();
+    this.wasmContext?.stopGame?.();
+    this.wasmContext = undefined;
+    this.gameInitialized = false;
   }
 
   private async initializeGameCanvas() {
@@ -60,13 +54,15 @@ class ScpFoundationSiteDirectorGameWidget extends GameWidget {
       const wasmBuffer = loadWasmModuleFromBase64(gameWasm);
       console.time('gameLoad'); // 384 ~ 1551 ms
       try {
-        destroy();
+        this.wasmContext = getWasmContext();
         /**
+         * Use `initAsync` for large wasm.
+         *
          * Can't use `initSync`, it cause error:
          * > RangeError: WebAssembly.Compile is disallowed on the main thread, if the buffer size is larger than 8MB. Use WebAssembly.compile, compile on a worker thread
          */
-        this.gameInstance = await initAsync(wasmBuffer);
-        startGame();
+        await this.wasmContext.initAsync(wasmBuffer);
+        this.wasmContext.startGame();
       } catch (error) {
         // https://users.rust-lang.org/t/getting-rid-of-this-error-error-using-exceptions-for-control-flow-dont-mind-me-this-isnt-actually-an-error/92209
         // this throw from `startGame()`, but don't hurt anything.
@@ -102,7 +98,7 @@ class ScpFoundationSiteDirectorGameWidget extends GameWidget {
       // TODO: store gamification events in a tiddler, and push them to game when game is initialized
       throw new Error('Game is not initialized yet!');
     }
-    setGamificationEvents(JSON.stringify(gamificationEventsJSON));
+    this.wasmContext?.setGamificationEvents?.(JSON.stringify(gamificationEventsJSON));
   }
 }
 

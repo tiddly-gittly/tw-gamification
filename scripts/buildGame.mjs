6792996e19a78be2636bbd8bfbc30a8c841d547e
lin onetwo <linonetwo012@gmail.com>
2024-03-05 03:03:56 +0800

    fix: version mismatch between wasm-bindgen and wasm-bindgen-cli

--- scripts/buildGame.mjs
+++ scripts/buildGame.mjs
@@ -14,3 +14,4 @@ const gamePath = path.resolve(__dirname, '../src/scp-foundation-site-director/ga
 cd(gamePath);
 await $`cargo build --release --target wasm32-unknown-unknown`;
 await $`wasm-bindgen --omit-default-module-path --reference-types --weak-refs --out-name game --out-dir wasm --target web target/wasm32-unknown-unknown/release/game.wasm --debug`;
+// await $`wasm-pack build --reference-types --weak-refs --out-name game --out-dir wasm --target web --dev`

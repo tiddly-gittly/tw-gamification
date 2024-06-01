a7665b94d59179cba7215f1ce929fff0fc38d05e
lin onetwo <linonetwo012@gmail.com>
2024-06-01 23:50:21 +0800

    refactor: move to https://github.com/linonetwo/scp-foundation-site-director

--- src/scp-foundation-site-director/game/type.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '$:/plugins/linonetwo/scp-foundation-site-director/game_bg.wasm' {
-  export * from './wasm/game_bg.wasm';
-}
--- src/scp-foundation-site-director/gameLoader/types.ts
+++
@@ -1,1 +0,0 @@
-export type * as IGameContext from '../game/wasm/game';

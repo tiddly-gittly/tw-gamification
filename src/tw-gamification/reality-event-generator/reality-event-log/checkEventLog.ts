541a042d4d2837c7fc3843278d24128baf0cb6da
lin onetwo <linonetwo012@gmail.com>
2026-04-21 00:17:56 +0800

    Feat/better ego store (#35)
    * feat: beautiry ego store task and add echarts to it
    
    * chore: add $:/plugins/linonetwo/hyper-table as Ego Store Lite dep

--- src/tw-gamification/reality-event-generator/reality-event-log/checkEventLog.ts
+++ src/tw-gamification/reality-event-generator/reality-event-log/checkEventLog.ts
@@ -1,4 +1,4 @@
-import { IActivityLogFile, LogFileTypes } from 'src/activity-log-tools/log-file-types/LogFileTypes';
+import { IActivityLogFile, LogFileTypes } from '../../../activity-log-tools/log-file-types/LogFileTypes';
 import { IRealityEventCacheCacheItem } from 'src/tw-gamification/types/RealityEventCacheTypes';
 import type { formatDuplicationFields } from '../deduplication/formatDuplicationFields';
 
